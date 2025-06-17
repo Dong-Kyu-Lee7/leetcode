7adc806247a21ee4c111557892ef7de44649cedc
DK <wwww0127@naver.com>
2025-06-17 12:36:53 +0900

    Time: 1192 ms (74.57%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
+# 2025/06/17 - 1068. Product Sales Analysis I
 select p.product_name, s.year, s.price
 from sales s
-inner join product p
+left join product p
 on p.product_id = s.product_id
+where s.year is not null and s.price is not null
